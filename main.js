9a31821aff2fdf5322fbd718cceb4e9028350bd7
StasZ2023 <159694096+StasZ2023@users.noreply.github.com>
2024-05-19 19:20:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 
-const mySwiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
   },
 
   // Navigation arrows
@@ -24,4 +25,9 @@ const mySwiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-})
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
